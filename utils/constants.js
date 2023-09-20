67b0084e4741250c5f0789cde5aa167f4c98b715
Abd Alfadeel Hamdy <abdalfadeelh@gmail.com>
2023-09-20 16:29:35 +0300

    Fix Bugs

--- utils/constants.js
+++ utils/constants.js
@@ -2,14 +2,12 @@ export const LICENSE_TYPE = {
   TRUCK: "Truck",
   CAR: "Car",
   CAR_AND_TRUCK: "Car&Truck",
-  CAR_AND_TRUCK3: "",
 };
 
 export const EMPLOYEE_STATUS = {
   DUTY: "duty",
   VACATION: "vacation",
   CANCEL: "cancelled",
-  CANCEL4: "",
 };
 
 export const NATIONALITIES = {};
